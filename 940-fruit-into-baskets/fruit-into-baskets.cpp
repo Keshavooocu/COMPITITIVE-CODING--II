40e448a925797cd99bd3eda0e882057845cf6ad2
Keshavooocu <keshav.cu05@gmail.com>
2026-08-13 12:40:04 +0530

    Time: 38 ms (75.29%) | Memory: 84.9 MB (34.64%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -3,7 +3,7 @@ public:
     int totalFruit(vector<int>& fruits) {
         unordered_map<int, int> basket;
         int left = 0;
-        int max_fruits = 0;
+        int maxf = 0;
 
         for (int right = 0; right < fruits.size(); ++right) {
             basket[fruits[right]]++;
@@ -15,10 +15,10 @@ public:
                 }
                 ++left;
             }
-            max_fruits = max(max_fruits, right - left + 1);
+            maxf = max(maxf, right - left + 1);
         }
 
-        return max_fruits;
+        return maxf;
         
     }
 };
